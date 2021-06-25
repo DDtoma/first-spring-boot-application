ffa264e0e5ad63b3aa9c787add49dac4e104e35f
llight <ll66456645@163.com>
2021-06-25 15:47:44 +0000

    使用kryo序列化redis

--- src/main/java/thinking/in/spring/boot/config/CacheConfig.java
+++ src/main/java/thinking/in/spring/boot/config/CacheConfig.java
@@ -1,9 +1,23 @@
 package thinking.in.spring.boot.config;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // KryoRedisSerializer 替换默认序列化
+        KryoRedisSerializer kryoRedisSerializer = new KryoRedisSerializer(Object.class);
+        redisTemplate.setValueSerializer(kryoRedisSerializer);
+        redisTemplate.setKeySerializer(kryoRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
 }
