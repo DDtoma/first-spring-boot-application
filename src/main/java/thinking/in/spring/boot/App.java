82f11d68808815477f9935ff051fca9170fffaeb
llight <ll66456645@163.com>
2021-06-25 12:35:46 +0000

    use static method

--- src/main/java/thinking/in/spring/boot/App.java
+++ src/main/java/thinking/in/spring/boot/App.java
@@ -1,15 +1,14 @@
 package thinking.in.spring.boot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(App.class);
-        System.out.println("hello world");
-        springApplication.setWebApplicationType(WebApplicationType.SERVLET);
-        springApplication.run(args);
+        SpringApplication.run(App.class, args);
+        // SpringApplication springApplication = new SpringApplication(App.class);
+        // springApplication.setWebApplicationType(WebApplicationType.SERVLET);
+        // springApplication.run(args);
     }
 }
