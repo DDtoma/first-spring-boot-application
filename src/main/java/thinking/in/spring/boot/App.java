a93737c0e87a5eb75b5884db347bb428f900e551
llight <ll66456645@163.com>
2021-06-25 12:32:50 +0000

    test

--- src/main/java/thinking/in/spring/boot/App.java
+++ src/main/java/thinking/in/spring/boot/App.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(App.class);
+        System.out.println("hello world");
         springApplication.setWebApplicationType(WebApplicationType.SERVLET);
         springApplication.run(args);
     }
